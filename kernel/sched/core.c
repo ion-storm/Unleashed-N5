db66922980cfda60624ae109f1a56f7356d8c18f
Viresh Kumar <viresh.kumar@linaro.org>
2013-06-06 18:31:22 -0500

    sched: Remove unused params of build_sched_domain()
    Date	Tue, 4 Jun 2013 16:50:19 +0530
    
    build_sched_domain() never uses parameter struct s_data *d and so passing it is
    useless.
    
    Remove it.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    (cherry picked from commit 01ef435f70b5a873ab2e556c3252777625de2b30)

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6492,7 +6492,7 @@ static int build_sched_domains(const struct cpumask *cpu_map,
 
 		sd = NULL;
 		for (tl = sched_domain_topology; tl->init; tl++) {
-			sd = build_sched_domain(tl, &d, cpu_map, attr, sd, i);
+			sd = build_sched_domain(tl, cpu_map, attr, sd, i);
 			if (tl == sched_domain_topology)
 				*per_cpu_ptr(d.sd, i) = sd;
 			if (tl->flags & SDTL_OVERLAP || sched_feat(FORCE_SD_OVERLAP))
