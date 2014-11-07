8a496eb7f495027caa2a6ce5e7f3f02511bc89bf
ion-storm <defconoii@gmail.com>
2014-11-07 16:39:29 -0500

    revert vibrator enhancements

--- drivers/platform/msm/qpnp-vibrator.c
+++ drivers/platform/msm/qpnp-vibrator.c
@@ -47,7 +47,7 @@ struct qpnp_vib {
 	int state;
 	int vtg_level;
 	int timeout;
-	spinlock_t lock;
+	struct mutex lock;
 };
 
 static struct qpnp_vib *vib_dev;
@@ -160,15 +160,9 @@ static void qpnp_vib_enable(struct timed_output_dev *dev, int value)
 {
 	struct qpnp_vib *vib = container_of(dev, struct qpnp_vib,
 					 timed_dev);
-	unsigned long flags;
 
-retry:
-	spin_lock_irqsave(&vib->lock, flags);
-	if (hrtimer_try_to_cancel(&vib->vib_timer) < 0) {
-		spin_unlock_irqrestore(&vib->lock, flags);
-		cpu_relax();
-		goto retry;
-	}
+	mutex_lock(&vib->lock);
+	hrtimer_cancel(&vib->vib_timer);
 
 	if (value == 0)
 		vib->state = 0;
@@ -180,9 +174,8 @@ retry:
 			      ktime_set(value / 1000, (value % 1000) * 1000000),
 			      HRTIMER_MODE_REL);
 	}
-	qpnp_vib_set(vib, vib->state);
-
-	spin_unlock_irqrestore(&vib->lock, flags);
+	mutex_unlock(&vib->lock);
+	schedule_work(&vib->work);
 }
 
 static void qpnp_vib_update(struct work_struct *work)
@@ -208,14 +201,9 @@ static enum hrtimer_restart qpnp_vib_timer_func(struct hrtimer *timer)
 {
 	struct qpnp_vib *vib = container_of(timer, struct qpnp_vib,
 							 vib_timer);
-	unsigned long flags;
-
-	spin_lock_irqsave(&vib->lock, flags);
 
 	vib->state = 0;
-	qpnp_vib_set(vib, vib->state);
-
-	spin_unlock_irqrestore(&vib->lock, flags);
+	schedule_work(&vib->work);
 
 	return HRTIMER_NORESTART;
 }
@@ -290,7 +278,7 @@ static int __devinit qpnp_vibrator_probe(struct spmi_device *spmi)
 		return rc;
 	vib->reg_en_ctl = val;
 
-	spin_lock_init(&vib->lock);
+	mutex_init(&vib->lock);
 	INIT_WORK(&vib->work, qpnp_vib_update);
 
 	hrtimer_init(&vib->vib_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
@@ -318,6 +306,7 @@ static int  __devexit qpnp_vibrator_remove(struct spmi_device *spmi)
 	cancel_work_sync(&vib->work);
 	hrtimer_cancel(&vib->vib_timer);
 	timed_output_dev_unregister(&vib->timed_dev);
+	mutex_destroy(&vib->lock);
 
 	return 0;
 }
