0c59e6abbd38ede2e0b1c94e6814acf22450b9fc
ion-storm <defconoii@gmail.com>
2014-11-09 02:06:56 -0500

    update 99defcon and cleaner

--- AnyKernel/tmp/cleaner.sh
+++ AnyKernel/tmp/cleaner.sh
@@ -4,6 +4,7 @@
 rm -rf /system/bin/thermald
 rm -rf /system/bin/mpdecision
 rm -rf /system/lib/hw/power.msm8974.so
+rm -rf /system/lib/hw/power.msm8960.so
 # clean init.d scripts
 INITD_DIR=/system/etc/init.d
 # Trinity
