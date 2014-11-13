2dd0d0ae4180c91d09dd808e67beb2dcac2a520f
ion-storm <defconoii@gmail.com>
2014-11-13 17:25:33 -0500

    Revert "ARM: mm: use phys_addr_t appropriately in p2v and v2p conversions"
    This reverts commit b517cbf90656e1545e69b391dc1a588b3566d92d.

--- arch/arm/include/asm/memory.h
+++ arch/arm/include/asm/memory.h
@@ -169,32 +169,22 @@ extern unsigned long __pv_phys_offset;
 	: "=r" (to)					\
 	: "r" (from), "I" (type))
 
-static inline phys_addr_t __virt_to_phys(unsigned long x)
+static inline unsigned long __virt_to_phys(unsigned long x)
 {
 	unsigned long t;
 	__pv_stub(x, t, "add", __PV_BITS_31_24);
 	return t;
 }
 
-static inline unsigned long __phys_to_virt(phys_addr_t x)
+static inline unsigned long __phys_to_virt(unsigned long x)
 {
 	unsigned long t;
 	__pv_stub(x, t, "sub", __PV_BITS_31_24);
 	return t;
 }
-
 #else
-
-static inline phys_addr_t __virt_to_phys(unsigned long x)
-{
-	return (phys_addr_t)x - PAGE_OFFSET + PHYS_OFFSET;
-}
-
-static inline unsigned long __phys_to_virt(phys_addr_t x)
-{
-	return x - PHYS_OFFSET + PAGE_OFFSET;
-}
-
+#define __virt_to_phys(x)	((x) - PAGE_OFFSET + PHYS_OFFSET)
+#define __phys_to_virt(x)	((x) - PHYS_OFFSET + PAGE_OFFSET)
 #endif
 #endif
 
@@ -229,14 +219,14 @@ static inline phys_addr_t virt_to_phys(const volatile void *x)
 
 static inline void *phys_to_virt(phys_addr_t x)
 {
-	return (void *)__phys_to_virt(x);
+	return (void *)(__phys_to_virt((unsigned long)(x)));
 }
 
 /*
  * Drivers should NOT use these either.
  */
 #define __pa(x)			__virt_to_phys((unsigned long)(x))
-#define __va(x)			((void *)__phys_to_virt((phys_addr_t)(x)))
+#define __va(x)			((void *)__phys_to_virt((unsigned long)(x)))
 #define pfn_to_kaddr(pfn)	__va((pfn) << PAGE_SHIFT)
 
 /*
